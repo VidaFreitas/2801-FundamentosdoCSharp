42ecb14c849044cf9bfb6e15816dadb171a3f224
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 14:09:50 -0300

    Reference Types

---
+++ 4. Net/MeuApp/ProgramValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class ProgramValue
+    {
+        static void Main(string[] args)
+        {
+            var arr = new string[2];
+            arr[0] = "Item 1";
+
+            var arr2 = arr;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            arr[0] = "Item 2";
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+        }
+    }
+}
