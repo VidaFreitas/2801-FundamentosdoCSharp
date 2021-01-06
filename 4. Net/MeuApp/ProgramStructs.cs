5617e100a60639a48723d8188f2f7a5a610d4430
Vida Valessa <vidavalessa65@gmail.com>
2021-01-06 14:45:54 -0300

    Product Structs

---
+++ 4. Net/MeuApp/ProgramStructs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class ProgramStructs
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Mouse gamer", 299.97);
+
+            Console.WriteLine(mouse.Id)
+            Console.WriteLine(mouse.Name)
+            Console.WriteLine(mouse.Price)
+        }
+    }
+    struct Product
+    {
+        public Product(int id, string, double Price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}
